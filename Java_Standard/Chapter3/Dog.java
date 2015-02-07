d89ac5da569de5478200fb05ada95ecbc131a69c
Wenxuan <wenxuanchen05@gmail.com>
2015-02-07 17:54:23 -0500

    第三章内容更新

---
+++ Java_Standard/Chapter3/Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    static int furColor;
+    float height;
+    float weight;
+
+    void catchMouse(Mouse m){
+	//m.scream();
+    }
+
+    public static void main (String[] args){
+	System.out.println(furColor);
+	Dog d = new Dog();
+	Mouse m = new Mouse();
+	d.catchMouse(m);
+    }
+
+}
+
+class Mouse {}
